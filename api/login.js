4429d760e41fb0ed4e2b951bccdfb5a283d28271
gabi <114922714+ivsgabi@users.noreply.github.com>
2025-02-11 13:40:08 +0100

    Delete api directory

--- api/login.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    const { CLIENT_ID, REDIRECT_URI } = process.env;
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=[COMPLETE-HERE]&response_type=code&redirect_uri=${encodeURIComponent([COMPLETE-HERE])}&scope=user-read-private user-read-email`;
-
-  // TO DO: Redirect the user to the authorization URL (1 line)
-  }
-  
